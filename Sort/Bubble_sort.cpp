65a9f6dda0e9fd084daa8b8360ae7457129efe3d
Tema <92046258+VegaMethadone@users.noreply.github.com>
2022-09-27 01:05:21 +0300

    Update Bubble_sort.cpp

--- Sort/Bubble_sort.cpp
+++ Sort/Bubble_sort.cpp
@@ -3,27 +3,15 @@
 using namespace std;
 
 int main() {
-    int str[17];
-
-    str[0] = 92;
-    str[1] = 32;
-    str[2] = 34;
-    str[3] = 53;
-    str[4] = 12;
-    str[5] = 53;
-    str[6] = 75;
-    str[7] = 24;
-    str[8] = 75;
-    str[9] = 24;
-    str[10] = 78;
-    str[11] = 67;
-    str[12] = 99;
-    str[13] = 67;
-    str[14] = 12;
-    str[15] = 43;
-    str[16] = 55;
-
-    int size = 16;
+    int size, i;
+    cin >> size;
+    
+    int str[size];
+    
+    while(i < size){
+        cin >>size[i++];
+    }  
+       
     int tmp;
     int bypass = 0;
 
