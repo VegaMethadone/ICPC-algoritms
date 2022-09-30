388634f5bf4436cf8000bfdc1feff06bd3030fb6
Tema <92046258+VegaMethadone@users.noreply.github.com>
2022-09-30 20:26:50 +0300

    Create bin_search_O(log n).cpp

---
+++ Sort/bin_search_O(log n).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; ++i){
+        a[i] = rand() % 100;
+    }
+
+    int num;
+    cin >> num;
+    int l = -1, r = a.size() - 1;
+    while (l + 1 < r){
+        int m = (l + r) / 2;
+        if(a[m] <  num){
+            l = m;
+        }
+        else{
+            r = m;
+        }
+    }
+
+    cout << "index is: " << r << "\n";
+}
