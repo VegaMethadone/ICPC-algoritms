217d9e97098f1607597345c47b7bddd18a47df4e
Tema <92046258+VegaMethadone@users.noreply.github.com>
2022-09-30 03:22:42 +0300

    Add files via upload

---
+++ math/fib.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+int fib(int num)
+{
+    if(num == 1){
+        return 1;
+    }
+    else if(num < 0){
+        return 0;
+    }
+    return fib(num - 1) + fib(num - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int res;
+    res = fib(n);
+    cout << res;
+}
